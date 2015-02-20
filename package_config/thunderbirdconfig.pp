9fedcfe588a22e7280ff8f1ac16509f6786bc63d
Jessie Lee <jessie@palantetech.coop>
2015-02-20 16:04:58 -0500

    changing up thunderbird config for architecture

--- package_config/thunderbirdconfig.pp
+++ package_config/thunderbirdconfig.pp
@@ -1,7 +1,7 @@
 #If this works and I think it will i may use this as basis for future configs. 
 class thunderbirdconfig {
   $thunderbirdpolicyjs = hiera('thunderbird:policyjs')
-  if $architecture == 'amd64' {
+  if $::architecture == 'x64' {
     $installpath = '%PROGRAMFILES32%/Mozilla Thunderbird'
   }
   else {
@@ -9,9 +9,9 @@ class thunderbirdconfig {
   }
   file_line { 'policyjs':
     path => "${installpath}/defaults/pref/policies.js", 
-    line => '$thunderbirdpolicyjs', }
+    line => $thunderbirdpolicyjs, }
   $thunderbirdconfig = hiera('thunderbird:config')
   file_line {'config':
     path => "${installpath}/",
-    line => "$thunderbirdconfig, }
+    line => $thunderbirdconfig, }
   }
