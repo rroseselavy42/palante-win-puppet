eabe9f8d0ea6fb65ed66f607829d027ed45e62d4
Jessie Lee <jessie@palantetech.coop>
2015-02-20 17:48:34 -0500

    fixes from attempted runs

--- package_config/firefoxconfig.pp
+++ package_config/firefoxconfig.pp
@@ -1,23 +1,23 @@
 class firefoxconfig {
-  if $::architecture = 'x86' {
+  if $::architecture == 'x86' {
   file_line { 'hideup':
-    path => '%PROGRAMFILESDIR%/Mozilla Firefox/mozilla.cfg',
+    path => "%PROGRAMFILESDIR%\\Mozilla Firefox\\mozilla.cfg",
     line => 'lockPref("browser.startup.homepage_override.mstone", "ignore");',
   
     }
   file_line { 'nodefault':
-    path => '%PROGRAMFILESDIR%/Mozilla Firefox/mozilla.cfg',
+    path => "%PROGRAMFILESDIR%\\Mozilla Firefox\\mozilla.cfg",
     line => 'lockPref("browser.shell.checkDefaultBrowser", false);',
 
     }
   file_line { 'noupdate':
-    path => '%PROGRAMFILESDIR%/Mozilla Firefox/mozilla.cfg',
+    path => "%PROGRAMFILESDIR%\\Mozilla Firefox\\mozilla.cfg",
     line => 'lockPref("app.update.enabled", false);',
     }
   }
   else {
     file_line { 'hideup':
-    path => '%PROGRAMFILESDIR32%/Mozilla Firefox/mozilla.cfg',
+    path => "%PROGRAMFILESDIR32%/Mozilla Firefox/mozilla.cfg",
     line => 'lockPref("browser.startup.homepage_override.mstone", "ignore");',
   
     }
