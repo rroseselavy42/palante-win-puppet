5720fa6912bf66e8ceda97be3f39c0a76e4d0070
Jessie Lee <jessie@palantetech.coop>
2015-02-20 16:03:41 -0500

    adding conditional fallback for firefox config. Also adding NTP windows timeservice lookup

---
+++ package_config/timeservice.pp
@@ -0,0 +1,18 @@
+# Class: Timeservice
+#
+#
+class Timeservice {
+  # checks registry values to turn on NTP
+  # uses hiera lookup to figure out what server to default to.
+  $ntpserver = hiera('ntp:server')
+  registry::value {'NTPon':
+    key   => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Parameters',
+    value => Type,
+    data  => 'NTP',
+  }
+  registry::value {'NTPserver':
+    key   => 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Parameters',
+    value => NtpServer,
+    data  => $ntpserver,
+  }
+}
