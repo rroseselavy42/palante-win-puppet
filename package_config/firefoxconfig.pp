5407912e74f1faff4bad806d73a2cc996914c907
Jessie Lee <jessie@palantetech.coop>
2015-02-26 19:15:10 -0500

    changing up firefox config for easier parsing

--- package_config/firefoxconfig.pp
+++ package_config/firefoxconfig.pp
@@ -1,34 +1,37 @@
 class firefoxconfig {
+  $hideup = "lockPref("browser.startup.homepage_override.mstone", "ignore");"
+  $nodefault = "lockPref("browser.shell.checkDefaultBrowser", false);"
+  $noupdate = "lockPref("app.update.enabled", false);"
   if $::architecture == 'x86' {
   file_line { 'hideup':
     path => 'C:/Program Files/Mozilla Firefox/mozilla.cfg,
-    line => 'lockPref("browser.startup.homepage_override.mstone", "ignore");',
+    line => $hideup,
   
     }
   file_line { 'nodefault':
     path => 'C:/Program Files/Mozilla Firefox/mozilla.cfg,
-    line => 'lockPref("browser.shell.checkDefaultBrowser", false);',
+    line => $nodefault,
 
     }
   file_line { 'noupdate':
     path => 'C:/Program Files/Mozilla Firefox/mozilla.cfg,
-    line => 'lockPref("app.update.enabled", false);',
+    line => $noupdate,
     }
   }
   else {
     file_line { 'hideup':
     path => 'C:/Program Files (x86)/Mozilla Firefox/mozilla.cfg',
-    line => 'lockPref("browser.startup.homepage_override.mstone", "ignore");',
+    line => $hideup,
   
     }
   file_line { 'nodefault':
     path => '%PROGRAMFILESDIR32%/Mozilla Firefox/mozilla.cfg',
-    line => 'lockPref("browser.shell.checkDefaultBrowser", false);',
+    line => $nodefault,
 
     }
   file_line { 'noupdate':
     path => '%PROGRAMFILESDIR32%/Mozilla Firefox/mozilla.cfg',
-    line => 'lockPref("app.update.enabled", false);',
+    line => $noupdate,
     }
   }
 }
