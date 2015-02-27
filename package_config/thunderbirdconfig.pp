d4cb61b6dc52817295fd19d29148c57fa196c332
Jessie Lee <jessie@palantetech.coop>
2015-02-26 20:47:01 -0500

    fixed config

--- package_config/thunderbirdconfig.pp
+++ package_config/thunderbirdconfig.pp
@@ -1,19 +1,32 @@
 #If this works and I think it will i may use this as basis for future configs. 
 class thunderbirdconfig {
   $thunderbirdpolicyjs = hiera('thunderbird:policyjs')
+
   if $::architecture == 'x64' {
+    file { "${installpath}/defaults/pref/policies.js":
+      ensure => file,
+    }
+    file { "${installpath}/thunderbird-custom.cfg":
+      ensure => file,
+    }
     $installpath = 'C:/Program Files (x86)/Mozilla Thunderbird'
    file_line { 'policyjs':
     path => "${installpath}/defaults/pref/policies.js", 
     line => $thunderbirdpolicyjs, }
   $thunderbirdconfig = hiera('thunderbird:config')
   file_line {'config':
-    path => "${installpath}/",
+    path => "${installpath}/thunderbird-custom.cfg",
     line => $thunderbirdconfig, }
 
   }
   else {
     $installpath = 'C:/Program Files/Mozilla Thunderbird'
+        file { "${installpath}/defaults/pref/policies.js":
+      ensure => file,
+    }
+    file { "${installpath}/thunderbird-custom.cfg":
+      ensure => file,
+    }
    file_line { 'policyjs':
     path => "${installpath}/defaults/pref/policies.js", 
     line => $thunderbirdpolicyjs, }
