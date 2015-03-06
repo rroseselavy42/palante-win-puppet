1284bd57e4ca98bd4aba5806f11fbe258d978c36
Jessie Lee <jessie@palantetech.coop>
2015-03-06 16:36:44 -0500

    cleaned up ultravnc.pp and added ocsagent.pp for testing

---
+++ package_installation/ocsagent.pp
@@ -0,0 +1,17 @@
+class ocsngagent_install {
+  $installsource = hiera('install:source')
+  $ocsngagentversion = hiera('ocsngagent:version')
+  $ocsserver = hiera('ocs:server')
+  package { "OCS Inventory NG Agent ${ocsngagentversion}":
+    ensure          => $ocsngagentversion,
+    source          => "${installsource}/ocsinventory/OCS-NG-Windows-Agent-Setup.exe",
+    install_options => ['/S', "/SERVER=${ocsserver}"],
+    notify          => Exec['cacert']
+
+  }
+  exec { 'cacert':
+    command     => 'cmd.exe  /c copy %SOFTWARE%\ocsinventory\cacert.pem "%PROGRAMDATA%\OCS Inventory NG\Agent"',
+    path        => $::path,
+    refreshonly => true,
+  }
+}
--- package_installation/ultravnc.pp
+++ package_installation/ultravnc.pp
@@ -3,52 +3,68 @@ class ultravnc_install {
   $ultravncversion = hiera('ultravnc:version')
   if $::architecture == 'x86' {
     package {"UltraVnc":
-      ensure => $ultravncversion,
-    #it would be great if 
-    #I could convert . to _ for this
-    #I will deal with it later YOLO 
-      source => "${installsource}/ultravnc/UltraVNC_1_2_05_X86_Setup.exe",
-      install_options => ['/verysilent','/no restart','loadinf="%SOFTWARE%\ultravnc\ultravnc.ini"'],
-      notify => Exec['addons']
-
-    }
+      ensure          => $ultravncversion,
+      #it would be great if 
+      #I could convert . to _ for this
+      #I will deal with it later YOLO 
+      source          => "${installsource}/ultravnc/UltraVnc_1205_x86.msi",
+      install_options => [ '/qn', '/norestart', {'SERVICE' => '1'}, {'CAD' => '1'}, {'SERVER' => '1'} ],
+      notify          => Exec['addons'],
+  }
       exec { 'addons':
-        command      => '%SOFTWARE%\\ultravnc\\UltraVNC_1_1_9_X86_Addons.exe /verysilent /loadinf="%SOFTWARE%\\ultravnc\\addons.inf"',
+        command     => 'cmd.exe %SOFTWARE%\ultravnc\UltraVNC_1_1_9_X86_Addons.exe /verysilent /loadinf="%SOFTWARE%\ultravnc\addons.inf"',
         refreshonly => true,
-        path => $::path,
-        notify => Exec['certinstall'],
-        require => Class['setsoftware'],
+        notify      => Exec['certinstall'],
+        path        => $::path,
       }
 
   
       exec { 'certinstall':
-        command      => '%comspec% /c %SOFTWARE%\\ultravnc\\certmgr.exe -add %SOFTWARE%\\ultravnc\\ultravnc_x86.cer -s -r localMachine trustedpublisher',
+        command     => 'cmd.exe /c %SOFTWARE%\\ultravnc\\certmgr.exe -add %SOFTWARE%\\ultravnc\\ultravnc_x86.cer -s -r localMachine trustedpublisher',
         refreshonly => true,
-        path => $::path,
-        notify => Exec['mirrordriver'],
+        path        =>  $::path,
+        notify      => Exec['mirrordriver'],
       }
         exec { 'mirrordriver':
-    command      => '"%SOFTWARE%\\ultravnc\\devcon_x86.exe" update "C:\\Program Files\\uvnc bvba\\UltraVNC\\driver\\driver\\mv2.inf" mv_hook_display_driver2',
+    command     => 'cmd.exe "%SOFTWARE%\ultravnc\devcon_x86.exe" update "C:\Program Files\uvnc bvba\UltraVNC\driver\driver\mv2.inf" mv_hook_display_driver2',
     refreshonly => true,
-    path => $::path,
-    notify     => Exec['setupdrv'],
+    path        => $::path,
+    notify      => Exec['setupdrv'],
+  }
+  }
+  exec { 'setupdrv':
+    command     => 'cmd.exe "%PROGRAMFILESDIR%\uvnc bvba\UltraVNC\driver\setupdrv.exe" installs',
+    refreshonly => true,
+    path        => $::path,
+    notify      =>  Service['uvnc_service'],
+  }
+  service { 'uvnc_service':
+    enable  => true,
+    ensure  => running,
+    notify  => Exec['softwarecad'],
+    require => Package['UltraVnc'],
+  }
+  exec { 'softwarecad':
+    command     => 'cmd.exe "%PROGRAMFILESDIR%\uvnc bvba\UltraVNC\winvnc.exe" -softwarecad',
+    refreshonly => true,
+    path        => $::path,
   }
     }
   else {
-    package {"UltraVnc":
-      ensure => $ultravncversion,
+    package {'UltraVnc':
+      ensure          => $ultravncversion,
       #it would be great if 
       #I could convert . to _ for this
       #I will deal with it later YOLO 
-      source => "${installsource}/ultravnc/UltraVnc_1205_X64.msi",
+      source          => "${installsource}/ultravnc/UltraVnc_1205_X64.msi",
       install_options => [ '/qn', '/norestart', {'SERVICE' => '1'}, {'CAD' => '1'}, {'SERVER' => '1'} ],
-      notify => Exec['addons'],
+      notify          => Exec['addons'],
   }
       exec { 'addons':
-        command      => 'cmd.exe %SOFTWARE%\ultravnc\UltraVNC_1_1_9_X64_Addons.exe /verysilent /loadinf="%SOFTWARE%\ultravnc\addons.inf"',
+        command     => 'cmd.exe %SOFTWARE%\ultravnc\UltraVNC_1_1_9_X64_Addons.exe /verysilent /loadinf="%SOFTWARE%\ultravnc\addons.inf"',
         refreshonly => true,
-        notify => Exec['certinstall'],
-        path => $::path,
+        notify      => Exec['certinstall'],
+        path        => $::path,
       }
 
   
