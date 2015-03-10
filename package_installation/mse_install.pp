bcc877d1610fb803b64ead64e821d04104f50389
Jessie Lee <jessie@palantetech.coop>
2015-03-10 17:46:45 -0400

    changed mse package to allow for registry changes using setacl

--- package_installation/mse_install.pp
+++ package_installation/mse_install.pp
@@ -7,7 +7,7 @@ class mse_install {
     $mseversion = hiera('mse:version')
   if $::architecture == 'x86' {
     package { 'Microsoft Security Essentials':
-        ensure          => $mseversion,
+        ensure          => present,
         source          => "${installsource}/microsoftsecurityessentials/mseinstall.exe",
         install_options => ['/s', '/o', '/runwgacheck'],
     }}
@@ -16,34 +16,66 @@ class mse_install {
         ensure          => $mseversion,
         source          => "${installsource}/microsoftsecurityessentials/mseinstall_64.exe",
         install_options => ['/s', '/o', '/runwgacheck'],
+        notify          => Exec['changeaclscan'],
     }
+  }
+  exec { 'changeaclscan':
+    command     => '%SOFTWARE%\microsoftsecurityessentials\SetACL.exe -on "hklm\software\Microsoft\Microsoft Antimalware\Scan" -ot reg -actn ace -ace "n:Administrators;p:full"',
+    refreshonly => true,
+    path        =>  $::path,
+    notify      => Exec['changeaclpaths'],
+  }
+  exec { 'changeaclpaths':
+    command     => '%SOFTWARE%\microsoftsecurityessentials\SetACL.exe -on "hklm\software\Microsoft\Microsoft Antimalware\Exclusions\Paths" -ot reg -actn ace -ace "n:Administrators;p:full"',
+    refreshonly => true,
+    path        =>  $::path,
   }
   #change registry to daily scan
   registry::value {'dailyscan':
-    key   => 'HKLM\Software\Microsoft\Microsoft Antimalware\Scan',
-    value => ScheduleDay,
-    type  => dword,
-    data  => '0x00000000',
+    key     => 'HKLM\Software\Microsoft\Microsoft Antimalware\Scan',
+    value   => ScheduleDay,
+    type    => dword,
+    data    => '0x00000000',
+    require => Exec['changeaclscan'],
     }
   #set time for daily scan
   registry::value {'scantime':
-    key   => 'HKLM\Software\Microsoft\Microsoft Antimalware\Scan',
-    value => ScheduleTime,
-    type  => dword,
-    data  => '0x000003c0',
+    key     => 'HKLM\Software\Microsoft\Microsoft Antimalware\Scan',
+    value   => ScheduleTime,
+    type    => dword,
+    data    => '0x000003c0',
+    require => Exec['changeaclpaths'],
+
     }
     #turn off only if idle
   registry::value {'onlyidle':
-    key   => 'HKLM\Software\Microsoft\Microsoft Antimalware\Scan',
-    value => ScanOnlyIfIdle,
-    type  => dword,
-    data  => '0x00000000',
+    key     => 'HKLM\Software\Microsoft\Microsoft Antimalware\Scan',
+    value   => ScanOnlyIfIdle,
+    type    => dword,
+    data    => '0x00000000',
+    require => Package['Microsoft Security Essentials'],
+
     }
   #exclude ultravnc
   registry::value {'except_ultravnc':
-    key   => 'HKLM\Software\Microsoft\Microsoft Antimalware\Exclusions\Paths',
-    value => 'C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe',
-    type  => dword,
-    data  => '0x00000000',
+    key     => 'HKLM\Software\Microsoft\Microsoft Antimalware\Exclusions\Paths',
+    value   => 'C:\Program Files\uvnc bvba\UltraVNC\winvnc.exe',
+    type    => dword,
+    data    => '0x00000000',
+    require => Package['Microsoft Security Essentials'],
+    notify  => Exec['restoreaclscan'],
+
     }
+  exec { 'restoreaclscan':
+    command     => '%SOFTWARE%\microsoftsecurityessentials\SetACL.exe -on "hklm\software\Microsoft\Microsoft Antimalware\Scan" -ot reg -actn ace -ace "n:Administrators;p:read"',
+    refreshonly => true,
+    path        =>  $::path,
+    notify      => Exec['restoreaclpaths'],
+  }
+  exec { 'restoreaclpaths':
+    command     => '%SOFTWARE%\microsoftsecurityessentials\SetACL.exe -on "hklm\software\Microsoft\Microsoft Antimalware\Exclusions\Paths" -ot reg -actn ace -ace "n:Administrators;p:read"',
+    refreshonly => true,
+    path        =>  $::path,
+
+  }
 }
