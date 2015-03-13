74779b53a598b9917955ec55e0f4a2d6fc8d3f09
Jessie Lee <jessie@palantetech.coop>
2015-03-13 11:41:27 -0400

    adding better variables for more automation

--- package_installation/adobereader_install.pp
+++ package_installation/adobereader_install.pp
@@ -1,10 +1,12 @@
 class adobereader_install {
 $installsource = hiera('install:source')
 $adobeversion = hiera('adobereader:version')
+$adobeversion2 = hiera('adobereader:packageversion')
 $versionnumeral = hiera('adobe:numeral')
     package{"Adobe Reader ${versionnumeral} (${adobeversion})":
         ensure          => $adobeversion,
-        source          => "${installsource}/adobereader/AdbeRdr${adobeversion}_en_US.exe",
+        source          => "${installsource}/adobereader/AdbeRdr${adobeversion2}_en_US.exe",
         install_options => ['/sALL'],
 
+}
 }
